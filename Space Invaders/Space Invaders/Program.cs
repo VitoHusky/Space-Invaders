2d40625e670eab06f44ae1785634c468342deeae
Marcel K. <admin@kallensrv.de>
2015-01-29 12:59:30 +0100

    Initial

---
+++ Space Invaders/Space Invaders/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Otter;
+
+namespace Space_Invaders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game("Space Invaders", 460, 480, 60, false);
+            game.Start();
+        }
+    }
+}
